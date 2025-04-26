1368b16041e3267594e71ada8e0ba7ba9959dfd7
rdelicad <rdelicad@gmail.com>
2025-04-26 17:42:08 +0200

    conexion API a demo trading

--- src/exchange/bybit_exchange.py
+++ src/exchange/bybit_exchange.py
@@ -1,29 +1,27 @@
 from pybit.unified_trading import HTTP
 import os
 from dotenv import load_dotenv
-import json
 
 load_dotenv()
 
-
 session = HTTP(
-    testnet=True,
+    testnet=False,  # No testnet
     api_key=os.getenv("BYBIT_API_KEY"),
     api_secret=os.getenv("BYBIT_API_SECRET"),
 )
+# Forzar endpoint demo trading
+session.endpoint = "https://api-demo.bybit.com"
 
 def ver_saldo():
     try:
         balance = session.get_wallet_balance(accountType="UNIFIED")
-        print(json.dumps(balance, indent=2))
-        """ coins = balance['result']['list'][0]['coin']
+        coins = balance['result']['list'][0]['coin']
         for coin in coins:
             if coin['coin'] == 'USDT':
                 print(f"Saldo disponible USDT: {coin['walletBalance']}")
-                print(f"Saldo total USDT (equity): {coin['equity']}") """
+                print(f"Saldo total USDT (equity): {coin['equity']}")
     except Exception as e:
         print(f"❌ Error: {str(e)}")
 
 if __name__ == "__main__":
     ver_saldo()
-
